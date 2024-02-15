4714800f063d79e1805d42f6b14461bcf4b5e557
ASafin666 <ed.crowley@yandex.ru>
2024-02-15 11:48:09 +0300

    first commit

---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,41 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void fillFormTest() {
+
+        open("/text-box");
+        $("#userName").setValue("Max");
+        $("#userEmail").setValue("Max@exmpl.com");
+        $("#currentAddress").setValue("some address");
+        $("#permanentAddress").setValue("another address");
+        $("#submit").click();
+
+        $("#output").$("name").shouldHave(text("Max"));
+        $("#output").$("email").shouldHave(text("Max@exmpl.com"));
+        $("#output").$("currentAddress").shouldHave(text("some address"));
+        $("#output").$("permanentAddress").shouldHave(text("another address"));
+    }
+
+}
+
+//Name:Max
+//Email:Max@exmpl.com
+//Current Address :some address
+//Permananet Address :another address
+
