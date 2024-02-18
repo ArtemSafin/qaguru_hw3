9ce734a9002517de94e47d2c49017cdd943f996b
ASafin666 <ed.crowley@yandex.ru>
2024-02-18 13:04:40 +0300

    add final corrections

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -4,8 +4,8 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -28,7 +28,7 @@ public class StudentRegistrationFormTest {
         $("#userEmail").setValue("charlie@gmail.com");
 
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("88005553535");
+        $("#userNumber").setValue("8800555353");
 
         $("#dateOfBirth-wrapper").click();
         $("#dateOfBirth-wrapper").shouldBe(Condition.visible).$(byText("July")).click();
@@ -41,7 +41,25 @@ public class StudentRegistrationFormTest {
 
         $("#uploadPicture").uploadFromClasspath("262.jpg");
 
+        $("#currentAddress").setValue("Some address");
+        $("#stateCity-wrapper").$(byText("Select State")).click();
+        $("#stateCity-wrapper").shouldHave(Condition.visible).$(byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText("Select City")).click();
+        $("#stateCity-wrapper").shouldHave(Condition.visible).$(byText("Noida")).click();
+
+        $("#submit").click();
+        $(".table-responsive").shouldHave(text("Artem Safin"),
+                (text("charlie@gmail.com")),
+                (text("Male")),
+                (text("8800555353")),
+                (text("17 July,1990")),
+                (text("Maths")),
+                (text("Music")),
+                (text("262.jpg")),
+                (text("Some address")),
+                (text("NCR Noida")));
+
+        $(".modal-footer").$(byText("Close")).click();
 
     }
-
 }
