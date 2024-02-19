7021ddfd667436594bb6742e22a13a6a8c0123cf
ASafin666 <ed.crowley@yandex.ru>
2024-02-19 18:46:44 +0300

    corrections on comments

--- src/test/java/tests/StudentRegistrationFormTest.java
+++ src/test/java/tests/StudentRegistrationFormTest.java
@@ -1,14 +1,12 @@
 package tests;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class StudentRegistrationFormTest {
     @BeforeAll
@@ -16,24 +14,27 @@ public class StudentRegistrationFormTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000;
     }
 
     @Test
     void studentRegistrationForm() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Artem");
-        $("#lastName").setValue("Safin");
+
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+
+        $("#firstName").setValue("Anton");
+        $("#lastName").setValue("LaVey");
         $("#userEmail").setValue("charlie@gmail.com");
 
         $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue("8800555353");
 
         $("#dateOfBirth-wrapper").click();
-        $("#dateOfBirth-wrapper").shouldBe(Condition.visible).$(byText("July")).click();
-        $("#dateOfBirth-wrapper").shouldBe(Condition.visible).$(byText("1990")).click();
-        $("#dateOfBirth-wrapper").shouldBe(Condition.visible).$(byText("17")).click();
+        $("#dateOfBirth-wrapper").$(byText("July")).click();
+        $("#dateOfBirth-wrapper").$(byText("1990")).click();
+        $("#dateOfBirth-wrapper").$(byText("17")).click();
 
         $("#subjectsContainer").click();
         $("#subjectsInput").setValue("Maths").pressEnter();
@@ -43,21 +44,22 @@ public class StudentRegistrationFormTest {
 
         $("#currentAddress").setValue("Some address");
         $("#stateCity-wrapper").$(byText("Select State")).click();
-        $("#stateCity-wrapper").shouldHave(Condition.visible).$(byText("NCR")).click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
         $("#stateCity-wrapper").$(byText("Select City")).click();
-        $("#stateCity-wrapper").shouldHave(Condition.visible).$(byText("Noida")).click();
+        $("#stateCity-wrapper").$(byText("Noida")).click();
 
         $("#submit").click();
-        $(".table-responsive").shouldHave(text("Artem Safin"),
-                (text("charlie@gmail.com")),
-                (text("Male")),
-                (text("8800555353")),
-                (text("17 July,1990")),
-                (text("Maths")),
-                (text("Music")),
-                (text("262.jpg")),
-                (text("Some address")),
-                (text("NCR Noida")));
+        $(".table-responsive")
+                .shouldHave(text("Anton LaVey"))
+                .shouldHave(text("charlie@gmail.com"))
+                .shouldHave(text("Male"))
+                .shouldHave(text("8800555353"))
+                .shouldHave(text("17 July,1990"))
+                .shouldHave(text("Maths"))
+                .shouldHave(text("Music"))
+                .shouldHave(text("262.jpg"))
+                .shouldHave(text("Some address"))
+                .shouldHave(text("NCR Noida"));
 
         $(".modal-footer").$(byText("Close")).click();
 
