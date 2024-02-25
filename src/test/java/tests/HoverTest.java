5695ec877320eae841fca4d9b93e78e935071534
ASafin666 <ed.crowley@yandex.ru>
2024-02-25 22:38:49 +0300

    commit hover test

---
+++ src/test/java/tests/HoverTest.java
@@ -0,0 +1,36 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class HoverTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+//        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000;
+    }
+
+    @Test
+    void openPopupWithHover() {
+        open("https://github.com");
+        $(withText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $(".application-main").shouldHave(text("To build, scale, and deliver secure software."));
+    }
+
+    @Test
+    void actionTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldBe(text("B"));
+    }
+
+}
