451af04e9ddac301622f9ab557efaed7d1819d72
SafinArtem <ed.crowley@yandex.ru>
2024-06-02 17:15:42 +0300

    Commit file parsing tests

---
+++ src/test/java/object/UserObject.java
@@ -0,0 +1,28 @@
+package object;
+
+public class UserObject {
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+
+    public String getLastname() {
+        return lastname;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    private String firstname;
+    private String lastname;
+    private String role;
+    private String login;
+}
