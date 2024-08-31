75d2bdff94ade7df94bfbbcca694478dd0717897
SafinArtem <ed.crowley@yandex.ru>
2024-08-31 18:54:35 +0300

    add remote on selenoid test

--- src/test/java/tests/RemoteRegistrationTest.java
+++ src/test/java/tests/RemoteRegistrationTest.java
@@ -17,6 +17,8 @@ public class RemoteRegistrationTest {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
     }
 
     @Test
